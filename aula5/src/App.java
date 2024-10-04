1194040deb741884e409b06e132497dead408867
Sabrina Silvestre da Luz <sabrinasilvestre279@gmail.com>
2024-10-04 14:27:39 +0100

    second commit

---
+++ aula5/src/App.java
@@ -0,0 +1,24 @@
+package src;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 1; i <= 1000; i++){
+
+            if (i % 3 == 0 && i % 5 == 0){
+                    sum += i;
+                    count ++;
+                    System.out.print(i + " ");
+            }
+
+            if (count == 5){
+                break;
+            }
+            
+        }
+        System.out.println("\nThe sum of the values is equals to " + sum);
+
+    }
+}
