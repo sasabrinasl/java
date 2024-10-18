b057f291fc0efeac12057c4c1bae7fe57898be4a
Sabrina Silvestre da Luz <sabrinasilvestre279@gmail.com>
2024-10-18 16:10:32 +0100

    atualização dos exercícios

---
+++ aula11/atividade27.java
@@ -0,0 +1,10 @@
+public class atividade27 {
+    public static void main(String[] args) {
+        String idade = "30";
+
+        int i = Integer.parseInt(idade);
+        float age = Float.parseFloat(idade);
+    
+        System.out.println("Valor int: " + i + "\nValor float: " + age);   
+    }
+}
