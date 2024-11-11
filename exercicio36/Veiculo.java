090496d75b3eb70c433d6036b7c2e67e905b4727
Sabrina Luz <120664960+sasabrinasl@users.noreply.github.com>
2024-11-11 17:00:27 +0000

    Exercicio 36

---
+++ exercicio36/Veiculo.java
@@ -0,0 +1,59 @@
+
+
+public abstract class Veiculo {
+    private String veiculo, cor, marca, modelo;
+
+    public Veiculo(String veiculo, String cor, String marca, String modelo){
+        this.veiculo = veiculo;
+        this.cor = cor;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getVeiculo(){
+        return veiculo;
+    }
+    public void setVeiculo(String veiculo){
+        this.veiculo = veiculo;
+    }
+
+    public String getCor(){
+        return cor;
+    }
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+
+    public String getmarca(){
+        return marca;
+    }
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getmodelo(){
+        return modelo;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public void exibirDetalhes(){
+        System.out.println("Veiculo: " + veiculo);
+        System.out.println("Cor: " + cor);
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+    }
+
+    public void acelerar(){
+        System.out.println("ACELERAAAAAA");
+    }
+
+    public void travar(){
+        System.out.println("TRAVAAAAAAAAAAAAAAAAAA!!!!!!");
+    }
+
+    public abstract void calcularPrecoAluguer(int dias);
+    
+}
